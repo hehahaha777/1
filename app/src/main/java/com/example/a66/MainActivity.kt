9ca2d59761bc7afff2fb0f27bcda268bc84c0df6
student <o1083025@gm.pu.edu.tw>
2021-03-22 14:28:29 +0800

    liubi

--- app/src/main/java/com/example/a66/MainActivity.kt
+++ app/src/main/java/com/example/a66/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.a66
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var counter:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //666666
+        txv.setOnClickListener(this)
     }
+
+
+
+    fun Add(v: View){
+        if (v == btn1){
+            counter++
+        }
+        else if(v==btn2){
+            counter+=2
+        }
+        else{
+            counter=0
+        }
+
+        txv.text = counter.toString()
+    }
+
+    override fun onClick(v: View?) {
+        counter+=2
+        txv.text = counter.toString()
+    }
+
+
 }
